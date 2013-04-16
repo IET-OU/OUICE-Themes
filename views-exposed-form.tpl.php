a7cdfd7aa65c5725467c39b7b7cf6ce2bc65d182
Lee Austin <lee@vnice.net>
2013-04-16 11:34:03 +0100

    Fixed issue with exposed filters

--- views-exposed-form.tpl.php
+++ views-exposed-form.tpl.php
@@ -1,8 +1,6 @@
 <?php
-// $Id: views-exposed-form.tpl.php,v 1.4.4.1 2009/11/18 20:37:58 merlinofchaos Exp $
 /**
- * @file views-exposed-form.tpl.php
- *
+ * @file
  * This template handles the layout of the views exposed filter form.
  *
  * Variables available:
@@ -10,10 +8,18 @@
  * - $widget->label: The visible label to print. May be optional.
  * - $widget->operator: The operator for the widget. May be optional.
  * - $widget->widget: The widget itself.
+ * - $sort_by: The select box to sort the view using an exposed form.
+ * - $sort_order: The select box with the ASC, DESC options to define order. May be optional.
+ * - $items_per_page: The select box with the available items per page. May be optional.
+ * - $offset: A textfield to define the offset of the view. May be optional.
+ * - $reset_button: A button to reset the exposed filter applied. May be optional.
  * - $button: The submit button for the form.
  *
  * @ingroup views_templates
  */
+
+ //    <input type="button" value=" print t('Reset') " onclick="javascript:$(this.form).clearForm();$(this.form).submit();" />
+
 ?>
 <?php if (!empty($q)): ?>
   <?php
@@ -22,10 +28,10 @@
     print $q;
   ?>
 <?php endif; ?>
-<div class="views-exposed-form slide-area" id="locations-slide-filters" style="display: none;">
-  <div class="views-exposed-widgets clear-block">
-    <?php foreach($widgets as $id => $widget): ?>
-      <div class="views-exposed-widget">
+<div class="views-exposed-form">
+  <div class="views-exposed-widgets clearfix">
+    <?php foreach ($widgets as $id => $widget): ?>
+      <div id="<?php print $widget->id; ?>-wrapper" class="views-exposed-widget views-widget-<?php print $id; ?>">
         <?php if (!empty($widget->label)): ?>
           <label for="<?php print $widget->id; ?>">
             <?php print $widget->label; ?>
@@ -39,12 +45,38 @@
         <div class="views-widget">
           <?php print $widget->widget; ?>
         </div>
+        <?php if (!empty($widget->description)): ?>
+          <div class="description">
+            <?php print $widget->description; ?>
+          </div>
+        <?php endif; ?>
       </div>
     <?php endforeach; ?>
-    <div class="views-exposed-widget">
-      <?php print $button ?>
-      <input type="button" value="<?php print t('Reset') ?>" onclick="javascript:$(this.form).clearForm();$(this.form).submit();" />
-      
+    <?php if (!empty($sort_by)): ?>
+      <div class="views-exposed-widget views-widget-sort-by">
+        <?php print $sort_by; ?>
+      </div>
+      <div class="views-exposed-widget views-widget-sort-order">
+        <?php print $sort_order; ?>
+      </div>
+    <?php endif; ?>
+    <?php if (!empty($items_per_page)): ?>
+      <div class="views-exposed-widget views-widget-per-page">
+        <?php print $items_per_page; ?>
+      </div>
+    <?php endif; ?>
+    <?php if (!empty($offset)): ?>
+      <div class="views-exposed-widget views-widget-offset">
+        <?php print $offset; ?>
+      </div>
+    <?php endif; ?>
+    <div class="views-exposed-widget views-submit-button">
+      <?php print $button; ?>
     </div>
+    <?php if (!empty($reset_button)): ?>
+      <div class="views-exposed-widget views-reset-button">
+        <?php print $reset_button; ?>
+      </div>
+    <?php endif; ?>
   </div>
 </div>
