1bab663bde263b1d7605839f9e673d06aa119d7d
Stuart Crouch <stuart.crouch@gmail.com>
2013-07-31 16:30:13 +0100

    Fixed bug that prevented all brand module created blocks from rendering. Updated .info file to signify new version

--- block--oubrand.tpl.php
+++ block--oubrand.tpl.php
@@ -1,7 +1,4 @@
 <?php
 
-//print '<div class="block">';
 include('edit-block.tpl.php');
-print (module_exists('oubrand') ? oubrand_replace_ou_tokens('', $content) : $content);
-//print $content;
-//print '</div>';
+print $content;
